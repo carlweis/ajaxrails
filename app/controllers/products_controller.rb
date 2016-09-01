56d5e5f90b0f1541d1364f98100f9557b2a00226
Carl Weis <carl@givecampus.com>
2016-09-01 03:02:28 -0500

    Cleanup products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,12 @@
 class ProductsController < ApplicationController
   helper_method :sort_column, :sort_direction
+  before_action :set_product, only: [:show, :edit, :destroy]
+  before_action :search_products, only: [:index]
 
   def index
-    @products = Product.search(params[:search]).order(sort_column + ' ' + sort_direction)
-                        .page(params[:page]).per(10)
   end
 
   def show
-    @product = Product.find_by_id(params[:id])
   end
 
   def new
@@ -15,7 +14,7 @@ class ProductsController < ApplicationController
   end
 
   def create
-    @product = Product.new(params[:product])
+    @product = Product.new(product_params)
     if @product.save
       flash[:notice] = 'Successfully updated product.'
       redirect_to @product
@@ -25,13 +24,27 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    @product = Product.find_by_id(params[:id])
     @product.destroy
     flash[:notice] = 'Successfully destroyed product.'
     redirect_to products_url
   end
 
   private
+    def product_params
+      params.require(:product)
+        .permit(:name, :cost, :price, :quantity,
+                :description, :state, :released_at)
+    end
+
+    def set_product
+      @product = Product.find_by_id(params[:id])
+    end
+
+    def search_products
+     @products = Product.search(params[:search])
+       .order(sort_column + ' ' + sort_direction)
+       .page(params[:page]).per(10)
+    end
 
     def sort_column
       Product.column_names.include?(params[:sort]) ? params[:sort] : 'released_at'
